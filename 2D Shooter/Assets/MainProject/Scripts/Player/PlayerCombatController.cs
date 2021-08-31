f657afc3a12c607ca8548860b1187e2ab9d2e318
Asigaka <asigakasaramadzai@yandex.ru>
2021-08-31 17:39:49 +0300

    Revert "Player Layer"
    This reverts commit f3d47c52217cc5d6665f65acdacd60ed788306d0.

--- 2D Shooter/Assets/MainProject/Scripts/Player/PlayerCombatController.cs
+++ 2D Shooter/Assets/MainProject/Scripts/Player/PlayerCombatController.cs
@@ -77,16 +77,13 @@ public class PlayerCombatController : MonoBehaviour
         {
             if (_reloaded)
             {
-                RaycastHit2D hit = Physics2D.Raycast(currentWeapon.FirePoint.position, currentWeapon.FirePoint.right, currentWeapon.Info.Distance, 7);
+                RaycastHit2D hit = Physics2D.Raycast(currentWeapon.FirePoint.position, currentWeapon.FirePoint.right, currentWeapon.Info.Distance);
 
-                if (hit.collider != null)
+                if (hit.collider.GetComponent<EnemyTarget>())
                 {
-                    if (hit.collider.GetComponent<EnemyTarget>())
-                    {
-                        EnemyTarget target = hit.collider.GetComponent<EnemyTarget>();
+                    EnemyTarget target = hit.collider.GetComponent<EnemyTarget>();
 
-                        Debug.Log(hit.collider.name);
-                    }
+                    Debug.Log(hit.collider.name);
                 }
 
                 _reloaded = false;
