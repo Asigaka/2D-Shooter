3014f51658abbaeb0151b2ad2928604b8bf87587
Asigaka <asigakasaramadzai@yandex.ru>
2021-08-31 17:06:05 +0300

    Shooting

--- 2D Shooter/Assets/MainProject/Scripts/Items/Weapon/WeaponInfo.cs
+++ 2D Shooter/Assets/MainProject/Scripts/Items/Weapon/WeaponInfo.cs
@@ -9,9 +9,11 @@ public class WeaponInfo : ScriptableObject
     [SerializeField] private int damage;
     [SerializeField] private float rateOfFire;
     [SerializeField] private float rotateSpeed;
+    [SerializeField] private float distance;
 
     public string NameWeapon { get => nameWeapon; set => nameWeapon = value; }
     public int Damage { get => damage; set => damage = value; }
     public float RateOfFire { get => rateOfFire; set => rateOfFire = value; }
     public float RotateSpeed { get => rotateSpeed; set => rotateSpeed = value; }
+    public float Distance { get => distance; set => distance = value; }
 }
--- 2D Shooter/Assets/MainProject/Scripts/Player/PlayerCombatController.cs
+++ 2D Shooter/Assets/MainProject/Scripts/Player/PlayerCombatController.cs
@@ -9,9 +9,13 @@ public class PlayerCombatController : MonoBehaviour
 
     [SerializeField] private List<EnemyTarget> enemyTargets = new List<EnemyTarget>();
 
+    private PlayerController _pc;
     private EnemyTarget _mainTarget;
+
+    private float _localRateOfFire;
     private bool _canRotate = false;
     private bool _canShoot = false;
+    private bool _reloaded = true;
     private bool _targetAimMode = false;
     private bool _defaultAimMode = true;
 
@@ -19,16 +23,23 @@ public class PlayerCombatController : MonoBehaviour
 
     private void Start()
     {
-
+        _pc = GetComponent<PlayerController>();
     }
 
     private void Update()
     {
         FindMainTarget();
 
-        if (currentWeapon != null && _mainTarget != null && _canRotate)
+        if (currentWeapon != null)
         {
-            RotateWeaponToMainTarget();
+            if (!_reloaded)
+                Reload();
+
+            if (_mainTarget != null)
+                RotateWeaponToMainTarget();
+
+            if (_canShoot)
+                Shoot();
         }
     }
 
@@ -57,7 +68,43 @@ public class PlayerCombatController : MonoBehaviour
     public void OnFireDown()
     {
         _canRotate = true;
+        _canShoot = true;
+    }
 
+    private void Shoot()
+    {
+        if (_canShoot)
+        {
+            if (_reloaded)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(currentWeapon.FirePoint.position, currentWeapon.FirePoint.right, currentWeapon.Info.Distance);
+
+                if (hit.collider.GetComponent<EnemyTarget>())
+                {
+                    EnemyTarget target = hit.collider.GetComponent<EnemyTarget>();
+
+                    Debug.Log(hit.collider.name);
+                }
+
+                _reloaded = false;
+            }
+        }
+    }
+
+    private void Reload()
+    {
+        if (_localRateOfFire <= 0 && !_reloaded)
+        {
+            _localRateOfFire = currentWeapon.Info.RateOfFire;
+
+            _reloaded = true;
+        }
+        else
+        {
+            _localRateOfFire -= Time.deltaTime;
+
+            _reloaded = false;
+        }
     }
 
     public void OnFireUp()
@@ -68,9 +115,21 @@ public class PlayerCombatController : MonoBehaviour
 
     private void RotateWeaponToMainTarget()
     {
-        Vector2 direction = _mainTarget.transform.position - currentWeapon.transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        currentWeapon.transform.rotation = Quaternion.Slerp(currentWeapon.transform.rotation, rotation, currentWeapon.Info.RotateSpeed * Time.deltaTime);
+        if (_canRotate)
+        {
+            Vector2 direction = _mainTarget.transform.position - currentWeapon.transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            currentWeapon.transform.rotation = Quaternion.Slerp(currentWeapon.transform.rotation, rotation, currentWeapon.Info.RotateSpeed * Time.deltaTime);
+        }
+        else
+        {
+            currentWeapon.transform.rotation = Quaternion.Slerp(currentWeapon.transform.rotation, Quaternion.Euler(0, 0, 0), currentWeapon.Info.RotateSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(currentWeapon.FirePoint.position, currentWeapon.FirePoint.right * currentWeapon.Info.Distance);
     }
 }
